0335684887e673a13928b02ae8e7565bc38fdfbc
stas <justforspam12345678@gmail.com>
2019-01-09 21:19:30 +0300

    trend splitter added

--- shit_for_ml1/trend_helpers.py
+++ shit_for_ml1/trend_helpers.py
@@ -51,5 +51,115 @@ def extremum_spliter(df):
     пика к определенному тренду сохраняется с помощью
     колонки ID
     """
-    #TODO
-    return None
+    trend_start_list=[]
+    trend_end_list=[]
+    importance_list=[]
+    k_list=[]
+    b_list=[]
+    dispersion_list=[]
+    absolute_trend_lenght_list=[]
+    r_squared_of_trend_list=[]
+    tops_count_list=[]
+    peaks_count_list=[]
+    ID_list=[]
+    ratings_list=[]
+    #merge parameters of peaks/tops to same column, while adding column with exptremum type
+    coordinates_X_list=[]
+    coordinates_Y_list=[]
+    height_list=[]
+    width_list=[]
+    HW_ratio_list=[]
+    extremum_type_list=[]
+    
+    #tops_coordinates=[] 
+    #tops_height=[]
+    #tops_width=[]
+    #tops_HW_ratio=[]
+    #peaks_coordinates=[]
+    #peaks_height=[]
+    #peaks_width=[]
+    #peaks_HW_ratio=[]
+    
+    for huindex, row in df.iterrows():
+        trend_start = row.trend_start
+        trend_end = row.trend_end
+        importance = row.importance
+        k = row.k
+        b = row.b
+        dispersion = row.dispersion
+        absolute_trend_lenght = row.absolute_trend_lenght
+        r_squared_of_trend = row.r_squared_of_trend
+        tops_count = row.tops_count
+        peaks_count = row.peaks_count
+        for cor,h,w,hw in itertools.zip_longest(row.tops_coordinates,
+                                         row.tops_height,
+                                         row.tops_width,
+                                         row.tops_HW_ratio):
+            # common features for the trend ID 
+            trend_start_list.append(trend_start)
+            trend_end_list.append(trend_end)
+            importance_list.append(importance)
+            k_list.append(k)
+            b_list.append(b)
+            dispersion_list.append(dispersion)
+            absolute_trend_lenght_list.append(absolute_trend_lenght)
+            r_squared_of_trend_list.append(r_squared_of_trend)
+            tops_count_list.append(tops_count)
+            peaks_count_list.append(peaks_count)
+            ID_list.append(row.ID)
+            ratings_list.append(row.ratings)
+    
+            # variable featuers of each extremum
+            coordinates_X_list.append(cor[0])
+            coordinates_Y_list.append(cor[1])
+            height_list.append(h)
+            width_list.append(w)
+            HW_ratio_list.append(hw)
+            extremum_type_list.append(1) # 1 for tops
+        for cor,h,w,hw in itertools.zip_longest(row.peaks_coordinates,
+                                     row.peaks_height,
+                                     row.peaks_width,
+                                     row.peaks_HW_ratio):
+            # common features for the trend ID 
+            trend_start_list.append(trend_start)
+            trend_end_list.append(trend_end)
+            importance_list.append(importance)
+            k_list.append(k)
+            b_list.append(b)
+            dispersion_list.append(dispersion)
+            absolute_trend_lenght_list.append(absolute_trend_lenght)
+            r_squared_of_trend_list.append(r_squared_of_trend)
+            tops_count_list.append(tops_count)
+            peaks_count_list.append(peaks_count)
+            ID_list.append(row.ID)
+            ratings_list.append(row.ratings)
+            
+            # variable featuers of each extremum
+            coordinates_X_list.append(cor[0])
+            coordinates_Y_list.append(cor[1])
+            height_list.append(h)
+            width_list.append(w)
+            HW_ratio_list.append(hw)
+            extremum_type_list.append(0) # 1 for tops
+        
+    df_unraveled = pd.DataFrame({
+        'trend_start': trend_start_list,
+        'trend_end': trend_end_list,
+        'importance': importance_list,
+        'k': k_list,
+        'b': b_list,
+        'dispersion': dispersion_list,
+        'absolute_trend_lenght': absolute_trend_lenght_list,
+        'r_squared_of_trend': r_squared_of_trend_list,
+        'tops_count': tops_count_list,
+        'peaks_count': peaks_count_list,
+        'ID': ID_list,
+        'coordinates_X': coordinates_X_list,
+        'coordinates_Y': coordinates_Y_list,
+        'height': height_list,
+        'width': width_list,
+        'HW_ratio': HW_ratio_list,
+        'extremum_type': extremum_type_list,
+        'ratings': ratings_list
+    })
+    return df_unraveled
